f6920e489c6237886c7a8174307e4ad4a9064f64
Eduardo <belmondoer@gmail.com>
2021-02-26 17:06:01 -0300

    Version 2.0.1
    Versión incompleta para revisión

--- futbol.ts
+++ futbol.ts
@@ -1,112 +1,99 @@
-class Arquero{
+class Humano{
 
-    private nombre:string = "";
-    private defensa = 0;
+    private nombre:string;
 
     constructor(nombre:string) {
         this.nombre = nombre;
         
     }
     getNombre():string { return this.nombre; };
-
-    getDefensa():number { return this.defensa; };
-
-    setdefensa() { this.defensa = Math.floor(Math.random()*10); };
-
-    setNombre(nombre:string) { this.nombre = nombre; };
-
-    atajar (tiroDelJugador:number,direccionTiro):boolean { 
-        this.setdefensa();
-        return tiroDelJugador > this.getDefensa()&& direccionTiro < 9? false : true;
-          }
-
  
 }
 
-class Jugador {
+export interface Division{
+    sector:string[],
+    valorDeDestreza:number[]
+}
 
-    private nombre:string = "";
-    private ataque = 0;
-    private pieHabil:string = ""
+const divisionesDelArco:Division = {
+    sector:[`Abajo al centro`,
+    `Abajo izquierda`,
+    `Abajo derecha`,
+    `Medio al centro`,
+    `Medio derecha`,
+    `Medio izquierda`,
+    `Arriba derecha`,
+    `Arriba izquierda`,
+    `Arriba al centro`,
+    `Afuera`,
+    ],
+    valorDeDestreza:[9,5,4,9,7,8,2,2,8,99]
+}  
 
-    constructor(nombre:string, pieHabil:string) {
-        this.nombre = nombre;
-        this.pieHabil = pieHabil;
+class Arquero extends Humano{
+
+    private defensa:number;
+
+    constructor(nombre:string,defensa:number) {
+        super(nombre);
+        this.defensa = defensa;
+        
     }
-    getNombre():string { return this.nombre; };
+    getNombre():string { return `${super.getNombre()} el arquero`; };
+
+    getDefensa():number { return this.defensa; };
+
+    atajar(fuerzaYdireccion:number[],divisionesDelArco:Division):string{
+        let defensa:number = (divisionesDelArco.valorDeDestreza[fuerzaYdireccion[1]]+ this.getDefensa())/2;
+        return fuerzaYdireccion[0]> defensa ? "goool!!!": "atajooo!!"
+
+    }
+   
+}
+
+class Delantero extends Humano{
+
+    private ataque:number;
+    private pieHabil:string;
+
+    constructor(nombre:string, ataque:number, pieHabil:string) {
+        super(nombre);
+        this.ataque = ataque;
+        this.pieHabil = pieHabil;
+        
+    }
+    getNombre():string { return `${super.getNombre()} el delantero`; };
 
     getAtaque():number { return this.ataque; };
 
-    setAtaque() { this.ataque = Math.floor(Math.random()*10+3); };
+    getPieHabil():string { return this.pieHabil; };
 
-    setNombre(nombre:string) { this.nombre = nombre; };
+    patear(ladoQueRecibe:string):number[]{
+        let fuerzaYdireccion: number[]=[];
+        let direccionDelTiro: number = Math.floor(Math.random()*10);
+        let penalizacion: number = ladoQueRecibe != this.pieHabil ? 0.6 : 1; 
+        fuerzaYdireccion.push(penalizacion*this.getAtaque(),direccionDelTiro);
+        return fuerzaYdireccion;
+    }
     
-    patear():number{
-        let pieQuePatea= Math.floor(Math.random()*2); 
-        let potenciaDeTiro= 0;
-        this.setAtaque();
-        potenciaDeTiro += pieQuePatea > 1? this.getAtaque():Math.floor(this.getAtaque()*0.8);
-        return potenciaDeTiro;
-    }
-   
-
 }
 
-class Arco {
 
-    private divisionesDelArco:Array<string>=[`Abajo al centro`,
-                                             `Abajo izquierda`,
-                                             `Abajo derecha`,
-                                             `Medio al centro`,
-                                             `Medio derecha`,
-                                             `Medio izquierda`,
-                                             `Arriba derecha`,
-                                             `Arriba izquierda`,
-                                             `Arriba al centro`,
-                                             `al Palo derecho`,
-                                             `al Palo izquierdo`,
-                                             `al Travesaño`,
-                                               ];
-    constructor(){ 
+function recibirPelota():string{ 
+    let ladoQueRecibe:string = Math.floor(Math.random()*10) >4 ? `derecho`:`izquierdo`;
+    return ladoQueRecibe;
+} 
 
-    };
-    getDivisiones():Array<string>{ return this.divisionesDelArco}; // no se si la voy a necesitar
-    getPosicion():number{
-        return Math.floor(Math.random()*this.divisionesDelArco.length);
-    }
-}
-function aJugar(){ 
-    const jugador = new Jugador("Jose","derecho");
-    const arquero = new Arquero("Juan");
-    const arco = new Arco();
-    let goles = 0;
-    let atajadas = 0;
-    let texto = ``;
-    while (goles<5 && atajadas<5 ){
-        
-        let patada = jugador.patear();
-        let direccionDelTiro= arco.getPosicion();
-        let atajada = arquero.atajar(patada,direccionDelTiro);
-        console.log(`La patada fue de ${patada}`);
-        console.log(`La direccion fue de ${direccionDelTiro}`);
-        console.log(`la atajada fue de ${arquero.getDefensa()}`);
-        console.log(`¿Atajo? ${atajada}`);
-        if (atajada){
-            texto = `                  Casi convierte el jugador ${jugador.getNombre()},
-                    pero el arquero ${arquero.getNombre()} estaba ahi.
-                    La pelota fue ${arco.getDivisiones()[direccionDelTiro]}. `
-                    atajadas++
-        } else {
-            texto = `                  El jugador ${jugador.getNombre()} convierte el gol,
-                    el arquero ${arquero.getNombre()} no pudo detener la pelota que
-                    fue ${arco.getDivisiones()[direccionDelTiro]}. `
-                    goles++;
-        }
-        console.log();
-        console.log (texto + `El marcador esta en ${goles} goles`)
-    } 
-    texto= atajadas>goles ? `Gano ${arquero.getNombre()} ${atajadas}-${goles}`:`Gano ${jugador.getNombre()} ${goles}-${atajadas}`;
-    console.log(texto);
-}
-
-aJugar();
+const delantero = new Delantero("Jose", 6, "derecho");
+const arquero = new Arquero("Juan",6);
+const ladoReceptor = recibirPelota();                               
+const patada = delantero.patear(ladoReceptor);
+const atajada = arquero.atajar(patada,divisionesDelArco);
+console.log();
+console.log();
+console.log();
+console.log(`La pelota llega a los pies del jugador ${delantero.getNombre()} desde el lado ${ladoReceptor}!!`);
+console.log();
+console.log(`Este patea con el pie ${delantero.getPieHabil()} con todas sus fuerzas hacia el arco!!!`);
+console.log();
+console.log(`El arquero realiza una maniobra para llegar a ${divisionesDelArco.sector[patada[1]]} y ...${atajada}`);
